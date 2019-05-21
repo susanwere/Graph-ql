863bcf0ebac4ef864a54de678333b09128914889
susanwere <blue.were@yahoo.com>
2019-05-21 15:30:22 +0300

    feat(links): define graphql mutations for links

---
+++ test/graphql/mutations/create_link_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+# Test for link mutations
+class Mutations::CreateLinkTest < ActiveSupport::TestCase
+  def perform(user: nil, **args)
+    Mutations::CreateLink.new(object: nil, context: {}).resolve(args)
+  end
+
+  test 'create a new link' do
+    link = perform(
+        url: 'http://google.com',
+        description: 'Google Platform',
+      )
+
+    assert link.persisted?
+    assert_equal link.url, 'http://google.com'
+    assert_equal link.description, 'Google Platform'
+  end
+end
